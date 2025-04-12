e4b21dcc7c136ef020f1a401136c0311e666f12e
draflame <phulam03@gmail.com>
2025-04-13 05:38:15 +0700

    add layout,api overview, datatable, get api datatable

--- lamngocthanhphu22650381/src/App.jsx
+++ lamngocthanhphu22650381/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Admin from './Admin'
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Overview from "./components/Overview";
+import Main from "./components/Main";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Admin/>
+      <Container
+        style={{
+          boxShadow: "0 0 5px rgba(0, 0, 0, 0.5)",
+        }}
+      >
+        <Row>
+          <Col xs="3" style={{ borderRight: "1px solid #e5e7eb" }}>
+            <Nav />
+          </Col>
+          <Col xs="9">
+            <Header />
+            <Main />
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
